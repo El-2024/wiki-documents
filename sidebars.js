3738743b190ecfb37a70ebc2958ae0eb85a4ffe0
elainedanwu <254693273@qq.com>
2025-09-18 16:14:14 +0800

    add: ac1

--- sidebars.js
+++ sidebars.js
@@ -4192,6 +4192,7 @@ const sidebars = {
                 //   id: 'Edge/NVIDIA_Jetson/Application/Robotics',
                 // },
                 items: [
+                  'Robotics/Robot_Sensor/Camera/Getting_Start_with_AC1',
                   'Robotics/Robot_Sensor/Camera/Getting_Start_with_Orbbec_Gemini2_3D_Camera',
                   'Robotics/Robot_Sensor/Camera/Orbbec_Depth_Camera',
                   'Robotics/Robot_Sensor/Camera/ORB_SLAM3_with_Orbbec_Gemini2',
